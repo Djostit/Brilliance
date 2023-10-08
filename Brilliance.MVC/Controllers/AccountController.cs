f7aaa4e1a52bb2ae1fa58e39afb14d4a9f0e8a9b
Djostit <djostit@mail.ru>
2023-10-08 16:06:32 +0500

    Добавьте файлы проекта.

---
+++ Brilliance.MVC/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Brilliance.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+            => View();
+        public IActionResult SignIn()
+            => View();
+        public IActionResult Profile()
+            => View();
+    }
+}
