35cb23c822d76606dda271f6701b84e9719062a4
Djostit <djostit@mail.ru>
2023-10-08 16:06:32 +0500

    Initial commit

---
+++ Brilliance.MVC/Controllers/PostController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Brilliance.MVC.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Posts()
+            => View();
+    }
+}
