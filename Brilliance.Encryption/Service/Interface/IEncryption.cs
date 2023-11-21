0cbbd252926db617b562dc0a3fd39125eb9d84c3
Djostit <djostit@mail.ru>
2023-11-21 16:48:30 +0500

    Delete an unused project

--- Brilliance.Encryption/Service/Interface/IEncryption.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Brilliance.Encryption.Service.Interface
-{
-    internal interface IEncryption
-    {
-        public byte[] EncryptData(string data);
-        public string DecryptData(byte[] encryptedData);
-    }
-}
